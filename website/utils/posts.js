21b20cc2c361c5471a5eb4f6832f3fd3b5a0e6b4
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-07-19 06:56:41 -0600

    terms and such

---
+++ website/utils/posts.js
@@ -0,0 +1,30 @@
+import { remark } from 'remark';
+import html from 'remark-html';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join('policies');
+
+export async function getPostData(id) {
+    const fullPath = path.join(postsDirectory, `${id}.md`);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+  
+    // Use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
+  
+    // Use remark to convert markdown into HTML string
+    const processedContent = await remark()
+        .use(html)
+        .process(matterResult.content);
+    const contentHtml = processedContent.toString();
+  
+    // Combine the data with the id and contentHtml
+    // also return the original markdown
+    return {
+        id,
+        markdown: matterResult.content,
+        contentHtml,
+        ...matterResult.data,
+    };
+}
