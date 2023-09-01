1b8f48f9e8532c50631d4f4eacb70b3b7abfe3e0
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-08-31 22:17:51 -0600

    Update index.js

--- js-api/index.js
+++ js-api/index.js
@@ -6,6 +6,10 @@ const api = express();
 // middleware
 api.use(express.json());
 api.use(express.urlencoded({ extended: true }));
+api.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 // import routers
 const authRouter = require('./routes/auth')
