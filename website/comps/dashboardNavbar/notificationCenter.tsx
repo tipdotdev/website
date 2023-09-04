1863c6e39cb85dba7435e2c5b29827951e13d011
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-09-03 21:51:25 -0600

    Update notificationCenter.tsx

--- website/comps/dashboardNavbar/notificationCenter.tsx
+++ website/comps/dashboardNavbar/notificationCenter.tsx
@@ -1,26 +1,13 @@
-import { useUser } from "@clerk/nextjs"
 import { useEffect, useState } from "react"
 import { FaBell } from "react-icons/fa"
 import NavNotif from "./notification"
 import { useNotifications } from "@novu/notification-center"
+import useUser from "../../hooks/useUser"
 
 export function NotificationCenter() {
 
-    const { isLoaded, isSignedIn, user } = useUser()
+    const { user, isAuthLoading } = useUser()
     const { notifications, fetchNextPage, hasNextPage, isLoading, isFetching, markAllNotificationsAsRead } = useNotifications();
-
-    // const { feedClient, useFeedStore } = useKnockFeed()
-    // const notifs = useFeedStore((state) => state.items)
-
-    // const knockApiKey = process.env.NEXT_PUBLIC_KNOCK_PUBLIC_API_KEY as string
-    // const knockFeedId = process.env.NEXT_PUBLIC_KNOCK_FEED_CHANNEL_ID as string
-
-    // useEffect(() => {
-    //     // Once the feedClient is available, calling .fetch() will
-    //     // populate the feed. In the background, a websocket is
-    //     // opened that will receive new messages in real time.
-    //     feedClient.fetch();
-    // }, [feedClient]);
 
     const [unreadCount, setUnreadCount] = useState(0);
     useEffect(() => {
@@ -48,7 +35,7 @@ export function NotificationCenter() {
         }, 5000);
     }
 
-    if (isLoaded && !isLoading) {
+    if (!isAuthLoading) {
 
         return (
             <div className="dropdown dropdown-end">
@@ -70,7 +57,7 @@ export function NotificationCenter() {
                     <p className="text-lg">Notifications</p>
                     <div className="divider mt-1 mb-1"></div>
                     <ul tabIndex={0} className="">
-                        {recentNotifs.length == 0 && (
+                        {recentNotifs?.length == 0 && (
                             <li>
                                 <p>
                                     No notifications yet.
