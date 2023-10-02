ba40a9201fd678dce70b86b84c10fb3aa490e954
ZerohRaG3 <kyledickey02@gmail.com>
2023-10-01 19:28:21 -0600

    fix v2

--- website/pages/dashboard/subscribers.tsx
+++ website/pages/dashboard/subscribers.tsx
@@ -1,5 +1,4 @@
 import { Inter } from "next/font/google";
-import { RedirectToSignIn, useUser } from "@clerk/nextjs";
 import DashboardTopNav from "@/comps/dashboardNavbar";
 import DashboardSidebar from "@/comps/dashboardSidebar";
 import SEOHead from "@/comps/seohead";
@@ -10,19 +9,6 @@ import DashboardSubs from "@/comps/dashboardSubscribers";
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Page() {
-    const { isLoaded, isSignedIn, user } = useUser();
-
-    if (!isSignedIn) {
-        <RedirectToSignIn />
-    }
-
-    if (!isLoaded) {
-        return (
-            <main className={`flex min-h-screen flex-col justify-center items-center px-10 ${inter.className}`} data-theme="dracula">
-                <span className="loading loading-spinner"></span>
-            </main>
-        )
-    }
 
     return (
         <main className={`flex min-h-screen flex-col justify-center items-center px-10 ${inter.className}`} data-theme="dracula">
@@ -33,10 +19,10 @@ export default function Page() {
 
             {/* <DashboardFooter /> */}
 
-            <DashboardSidebar 
+            {/* <DashboardSidebar 
                 content={ <DashboardSubs user={user} /> }
                 user={ user }
-            />
+            /> */}
 
         </main>
     )
