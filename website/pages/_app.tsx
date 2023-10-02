98904db877e74ee88b0adf63f70ec1ed922de2db
ZerohRaG3 <kyledickey02@gmail.com>
2023-10-01 19:23:22 -0600

    Update _app.tsx

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,7 +1,5 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import { dark, neobrutalism } from '@clerk/themes';
-import { ClerkProvider } from '@clerk/nextjs'
 import { GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
