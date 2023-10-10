e91cfe77d6bb8b71dc9ea55cabde790f4a3b68a5
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-10-10 03:00:48 -0600

    plausible v3

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -6,7 +6,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleAnalytics trackPageViews />
-      <script defer data-domain="tip.dev" src="https://plausible-analytics-production-d712.up.railway.app/js/script.js"></script>
+      <script
+        src="/plausible/js/script.js"
+        data-api="/plausible/api/event"
+        data-domain="tip.dev"
+      ></script>
       <Component {...pageProps} />
     </>
   )
