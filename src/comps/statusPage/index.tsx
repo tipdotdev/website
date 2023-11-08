c5ec9dc1bc7f0bfc069e8f213cb4bdda3d8393e9
PapaRaG3 <41174949+dickeyy@users.noreply.github.com>
2023-11-08 11:07:23 -0700

    added status badge

--- src/comps/statusPage/index.tsx
+++ src/comps/statusPage/index.tsx
@@ -2,12 +2,12 @@ export default function StausPageBadge(props:any) {
 
     return (
         <>
-            {/* <iframe 
+            <iframe 
                 src="https://status.tip.dev/badge?theme=dark" 
                 className="border-0 prose" 
                 height={30}
                 width={184}
-            /> */}
+            />
         </>
     )
 }
