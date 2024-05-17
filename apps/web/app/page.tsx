5007e2601ee925fe9354957620987a829d71eca3
Kyle Dickey <41174949+dickeyy@users.noreply.github.com>
2024-05-17 06:10:56 -0600

    fix coming soon

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar";
 import SmallFooter from "@/components/small-footer";
 
 export default function Page() {
-    const comingSoon = false;
+    const comingSoon = true;
     return (
         <main className="bg-dot-white/[0.2] flex min-h-screen flex-col items-center justify-center p-4 sm:p-24">
             <Navbar active="home" comingSoon={comingSoon} />
