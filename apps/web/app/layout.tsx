6273f6a5a5e85a1f7ea93c086522c244a66d1d0e
Kyle Dickey <kyledickey02@gmail.com>
2025-01-14 18:27:58 -0700

    web: add metadata and analytics

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -13,8 +13,28 @@ const fontSerif = FontSerif({
 });
 
 export const metadata: Metadata = {
-    title: "tip.dev",
-    description: "tip.dev"
+    title: "tip.dev coming soon",
+    description:
+        "A platform for developers to get tipped by fans of their work. Embed a link anywhere and get paid from almost everywhere. Coming soon.",
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        url: "https://tip.dev",
+        title: "tip.dev coming soon",
+        description:
+            "A platform for developers to get tipped by fans of their work. Embed a link anywhere and get paid from almost everywhere. Coming soon.",
+        images: [
+            {
+                url: "https://tip.dev/images/png/og-image.png",
+                width: 1200,
+                height: 630
+            }
+        ]
+    },
+    robots: "noindex, nofollow",
+    referrer: "no-referrer-when-downgrade",
+    keywords:
+        "tip.dev, coming soon, tip, dev, tipdev, developers, devs, ko-fi, buymeacoffee, link in bio, money, freelance, web dev"
 };
 
 export default function RootLayout({
@@ -24,6 +44,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <script defer data-domain="tip.dev" src="https://a.kyle.so/js/script.js"></script>
+            </head>
             <body
                 className={`min-h-screen overflow-auto bg-background font-sans antialiased ${
                     (fontSans.variable, fontSerif.variable)
